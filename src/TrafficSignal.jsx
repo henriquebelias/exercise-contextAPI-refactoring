3bb451e8134a0aee756df5a09a758451ac58dc57
Henrique Brito Elias <henriquebelias@gmail.com>
2021-04-13 16:01:01 -0300

    exercise two

--- src/TrafficSignal.jsx
+++ src/TrafficSignal.jsx
@@ -1,9 +1,7 @@
 // src/TrafficSignal.jsx
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { changeSignal } from './redux/actionCreators';
+import React, { useContext } from 'react';
+import { CarContext } from './context';
 import redSignal from './images/redSignal.jpeg';
 import yellowSignal from './images/yellowSignal.jpeg';
 import greenSignal from './images/greenSignal.jpeg';
@@ -15,7 +13,8 @@ const renderSignal = (signalColor) => {
   return null;
 };
 
-const TrafficSignal = ({ signalColor, changeSignal }) => {
+const TrafficSignal = () => {
+  const { signalColor, changeSignal } = useContext(CarContext);
   return (
     <div>
       <div className="button-container">
@@ -34,15 +33,4 @@ const TrafficSignal = ({ signalColor, changeSignal }) => {
   );
 };
 
-const mapStateToProps = (state) => ({
-  signalColor: state.trafficReducer.signal.color
-});
-
-const mapDispatchToProps = { changeSignal };
-
-TrafficSignal.propTypes = {
-  changeSignal: PropTypes.func.isRequired,
-  signalColor: PropTypes.string.isRequired,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(TrafficSignal);
+export default TrafficSignal;
